8cc3ad3b637ff5f8634b8d09d744608ba9927fa5
David Franko <david.franko1998@gmail.com>
2020-06-01 14:13:08 -0700

    Some import fixed

--- src/main/java/gateway/websocket/WebsocketClient.java
+++ src/main/java/gateway/websocket/WebsocketClient.java
@@ -2,9 +2,6 @@ package gateway.websocket;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import gateway.model.MeasurementValues;
-import gateway.service.RequestService;
-import gateway.service.RequestServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
